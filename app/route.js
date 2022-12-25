841e6d30e3e204d045d92b751ed5b5ee36fa9bea
Damiano Ravalico <damianoravalico.uni@gmail.com>
2022-12-25 11:22:08 +0100

    Now you can search for a user by id. Some changes to the previous API

--- app/route.js
+++ app/route.js
@@ -1,45 +1,36 @@
+const { response } = require("express");
 const express = require("express");
 const router = express.Router();
 const db = require("./db.js");
 
-const CollectionsNames = {
+const DbCollectionsNames = {
     Users: "users",
     Messages: "messages",
 };
 
+router.delete("/dropuserdb", async (req, res) => {
+    const mongoDb = db.getDb();
+    await mongoDb.collection(DbCollectionsNames.Users).drop();
+    res.send("ok");
+});
+
 router.post("/auth/signup", async (req, res) => {
     const mongoDb = db.getDb();
     const userToInsert = req.body;
-    const users = await mongoDb
-        .collection(CollectionsNames.Users)
-        .find()
-        .toArray();
-    isAlreadyInDb = false;
-    if (typeof users !== "undefined") {
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].username === userToInsert.username) {
-                isAlreadyInDb = true;
-                break;
-            }
-        }
-    }
-    if (isAlreadyInDb) {
-        res.send("error");
-    } else {
-        await mongoDb
-            .collection(CollectionsNames.Users)
-            .insertOne(userToInsert);
-        if (typeof users !== "undefined") {
-            res.send(users);
-        } else {
-            res.send(userToInsert);
-        }
-    }
+    await mongoDb.collection(DbCollectionsNames.Users).insertOne(userToInsert);
+    delete userToInsert._id;
+    res.json(userToInsert);
 });
 
 router.post("/auth/signin", (req, res) => {});
 
-router.get("/social/users/:id", (req, res) => {});
+router.get("/social/users/:id", async (req, res) => {
+    const mongoDb = db.getDb();
+    const userWithId = await mongoDb
+        .collection(DbCollectionsNames.Users)
+        .findOne({ id: parseInt(req.body.id) });
+    res.json(userWithId);
+});
 
 router.get("/social/messages/:userId", (req, res) => {});
 
