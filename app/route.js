81b255867ebaac17827d14e0b4e8e5c209a62f48
Damiano Ravalico <damianoravalico.uni@gmail.com>
2022-12-26 18:38:46 +0100

    Now you can search a user by its id

--- app/route.js
+++ app/route.js
@@ -16,6 +16,15 @@ function generateJWT(username, hashedPassword) {
     return jwt.sign({ username, hashedPassword }, SECRET_KEY_JWT);
 }
 
+router.get("/all", async (req, res) => {
+    const mongoDb = db.getDb();
+    const users = await mongoDb
+        .collection(DbCollectionsNames.Users)
+        .find()
+        .toArray();
+    res.send(users);
+});
+
 router.delete("/dropuserdb", async (req, res) => {
     const mongoDb = db.getDb();
     await mongoDb.collection(DbCollectionsNames.Users).drop();
@@ -26,8 +35,8 @@ router.post("/auth/signup", async (req, res) => {
     const mongoDb = db.getDb();
     const userToInsert = req.body;
     userToInsert.password = await bcrypt.hash(req.body.password, 10);
-    userToInsert.followers = 0;
-    userToInsert.following = 0;
+    userToInsert.followers = "";
+    userToInsert.following = "";
     const usersCollectionElements = await mongoDb
         .collection(DbCollectionsNames.Users)
         .find()
@@ -47,15 +56,36 @@ router.post("/auth/signup", async (req, res) => {
         .json(userToInsert);
 });
 
-router.post("/auth/signin", (req, res) => {
+router.post("/auth/signin", async (req, res) => {
+    const mongoDb = db.getDb();
+    const userToLogin = req.body;
+    const user = await mongoDb
+        .collection(DbCollectionsNames.Users)
+        .findOne({ username: userToLogin.username });
+    console.log(userToLogin);
+    if (user && (await bcrypt.hash(userToLogin.password, 10))) {
+        const token = generateJWT(userToLogin.username, userToLogin.password);
+        res.cookie("jwtoken", token, {
+            maxAge: 1296000000,
+            httpOnly: true,
+        })
+            .status(200)
+            .json(userToLogin);
+    } else {
+        res.status(400).send("Invalid Credentials");
+    }
 });
 
 router.get("/social/users/:id", async (req, res) => {
     const mongoDb = db.getDb();
     const userWithId = await mongoDb
         .collection(DbCollectionsNames.Users)
-        .findOne({ id: parseInt(req.body.id) });
-    res.status(200).json(userWithId);
+        .findOne({ id: parseInt(req.params.id) });
+    if (userWithId) {
+        res.status(200).json(userWithId);
+    } else {
+        res.status(400).send("User not found");
+    }
 });
 
 router.get("/social/messages/:userId", (req, res) => {});
