0c6fae32a0d14f4e86b43d4b50df8ca4ff2e4f5c
Damiano Ravalico <damianoravalico.uni@gmail.com>
2022-12-29 09:36:42 +0100

    user-schema update

--- app/db/user-schema.js
+++ app/db/user-schema.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new mongoose.Schema({
     id: {
         type: Number,
         required: true,
+        unique: true,
     },
     name: {
         type: String,
@@ -28,6 +31,33 @@ const UserSchema = new mongoose.Schema({
     followers: [Number],
     following: [Number],
 });
+
+UserSchema.pre("save", function (next) {
+    var user = this;
+    if (!user.isModified("password")) {
+        return next();
+    }
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+    // https://stackoverflow.com/questions/14588032/mongoose-password-hashing
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 const User = mongoose.model("User", UserSchema);
 
