55049355c2eb825b97a6f5d7a767730ca59485d0
Damiano Ravalico <damianoravalico.uni@gmail.com>
2023-01-11 09:53:16 +0100

    New message for unhandable error page

--- frontend/src/views/Error.vue
+++ frontend/src/views/Error.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="mx-auto vh-100">
+    <div class="mx-auto w-100">
         <h1 class="display-4">Oops! Something went wrong</h1>
-        <div class="bordered-top row justify-content-center pt-4">
-            <p class="square centerd">
-                Some action has caused a fatal error. Check the JS console for technical details.
+        <div class="bordered-top pt-4">
+            <p>
+                Some action has caused a fatal error. Check the JS console for technical details or try to refresh the
+                page.
             </p>
         </div>
     </div>
